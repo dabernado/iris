81ee9167e069b4d347357d608fa114da9d8c5873
David Bernado <davidabernado@gmail.com>
2022-06-28 11:09:38 -0700

    implement RawPtr

--- src/mem/api.rs
+++ src/mem/api.rs
@@ -30,10 +30,6 @@ impl From<BlockError> for AllocError {
     }
 }
 
-pub struct RawPtr<T: Sized> {
-    ptr: NonNull<T>,
-}
-
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum SizeClass {
@@ -73,4 +69,39 @@ pub trait AllocHeader: Sized {
 pub fn alloc_size_of(object_size: usize) -> usize {
     let align = size_of::<usize>();
     (object_size + (align - 1)) & !(align - 1)
+}
+
+/*
+ * RawPtr API
+ */
+pub struct RawPtr<T: Sized> {
+    ptr: NonNull<T>,
+}
+
+impl<T: Sized> RawPtr<T> {
+    pub fn new(ptr: *const T) -> RawPtr<T> {
+        RawPtr {
+            ptr: unsafe { NonNull::new_unchecked(ptr as *mut T) },
+        }
+    }
+
+    pub fn as_ptr(self) -> *const T { self.ptr.as_ptr() }
+    pub fn as_word(self) -> usize { self.ptr.as_ptr() as usize }
+    pub fn as_untyped(self) -> NonNull<()> { self.ptr.cast() }
+    pub unsafe fn as_ref(&self) -> &T { self.ptr.as_ref() }
+    pub unsafe fn as_mut_ref(&mut self) -> &mut T { self.ptr.as_mut() }
+}
+
+impl<T: Sized> Clone for RawPtr<T> {
+    fn clone(&self) -> RawPtr<T> {
+        RawPtr { ptr: self.ptr }
+    }
+}
+
+impl<T: Sized> Copy for RawPtr<T> {}
+
+impl<T: Sized> PartialEq for RawPtr<T> {
+    fn eq(&self, other: &RawPtr<T>) -> bool {
+        self.ptr == other.ptr
+    }
 }
