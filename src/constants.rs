b5a9b88da04a6df5d88f1b41f80965848ee37716
David Bernado <davidabernado@gmail.com>
2022-09-06 20:13:52 -0700

    add elt/clt iso

--- src/constants.rs
+++ src/constants.rs
@@ -46,20 +46,22 @@ pub const OP_RR: u8 = 18;
 pub const OP_RL: u8 = !OP_RR ^ (OP_MASK as u8);
 pub const OP_RRI: u8 = 19;
 pub const OP_RLI: u8 = !OP_RRI ^ (OP_MASK as u8);
-pub const OP_CALL: u8 = 22;
+pub const OP_ELT: u8 = 20;
+pub const OP_CLT: u8 = !OP_ELT ^ (OP_MASK as u8);
+pub const OP_CALL: u8 = 23;
 pub const OP_UNCALL: u8 = !OP_CALL ^ (OP_MASK as u8);
-pub const OP_START: u8 = 23;
+pub const OP_START: u8 = 24;
 pub const OP_END: u8 = !OP_START ^ (OP_MASK as u8);
-pub const OP_SYSC: u8 = 24;
+pub const OP_SYSC: u8 = 25;
 pub const OP_RSYSC: u8 = !OP_SYSC ^ (OP_MASK as u8);
 
 // C-Type
 pub const OP_EXPF: u8 = 9;
 pub const OP_COLF: u8 = !OP_EXPF ^ (OP_MASK as u8);
-pub const OP_SUMC: u8 = 20;
-pub const OP_SUMC_R: u8 = !OP_SUMC ^ (OP_MASK as u8);
-pub const OP_PRODC: u8 = 21;
-pub const OP_PRODC_R: u8 = !OP_PRODC ^ (OP_MASK as u8);
+pub const OP_SUMS: u8 = 21;
+pub const OP_SUME: u8 = !OP_SUMS ^ (OP_MASK as u8);
+pub const OP_PRODS: u8 = 22;
+pub const OP_PRODE: u8 = !OP_PRODS ^ (OP_MASK as u8);
 
 // Maximums/Minimums
 pub const MAX_ITYPE_FIELD: u32 = 67108864;
