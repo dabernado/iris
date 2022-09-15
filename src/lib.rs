0865d8b928dc4fe67e03529830aa4e922ba8764d
David Bernado <davidabernado@gmail.com>
2022-09-15 13:20:06 -0700

    make some modules private

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #![feature(exclusive_range_pattern)]
 #![feature(ptr_const_cast)]
 
-pub mod alloc;
+mod alloc;
+mod array;
+mod context;
+mod printer;
+mod types;
 pub mod safeptr;
 pub mod data;
-pub mod types;
 pub mod memory;
-pub mod array;
 pub mod bytecode;
-pub mod context;
 pub mod constants;
 pub mod error;
-pub mod printer;
 pub mod op;
 pub mod vm;
